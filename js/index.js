68f9bab6e14df106c0255b64a9e71ae454214c9d
Illia Shcherbak <illiashcherbak6@gmail.com>
2023-11-15 18:39:27 +0200

    now you can add a task to the page by saving it to local storage. also i created the function to render the page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+import {
+  getTasksLocalStorage,
+  setTasksLocalStorage,
+  generateUniqueId,
+  //initSortableList,
+  updateListTasks,
+} from './utils.js';
+
 const form = document.querySelector('.form');
 const textareaForm = document.querySelector('.form__textarea');
 const buttonSendForm = document.querySelector('.form__send-btn');
@@ -7,3 +15,36 @@ const buttonCancel = document.querySelector('.form__cancel-btn');
 const output = document.querySelector('.output');
 let editId = null;
 let isEditTask = false;
+
+updateListTasks();
+
+//! All listeners
+form.addEventListener('submit', sendTask);
+
+//! All functions
+function sendTask(event) {
+  event.preventDefault();
+
+  const task = textareaForm.value.trim().replace(/\s+/g, ' ');
+
+  if (!task) {
+    return alert(`This field shouldn't be empty`);
+  }
+
+  const arrayTasksLS = getTasksLocalStorage();
+
+  arrayTasksLS.push({
+    id: generateUniqueId(),
+    task,
+    done: false,
+    pinned: false,
+    position: 1000,
+  });
+
+  setTasksLocalStorage(arrayTasksLS);
+  updateListTasks();
+
+  form.reset();
+}
+
+//sendTask(event);
